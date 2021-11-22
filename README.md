57e4822245195b6a311584d8fb2a6e019c578a58
FrancescoSaverioZuppichini <francesco.zuppichini@gmail.com>
2021-11-22 15:54:03 +0000

    data types

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# datasets
-A comprehensive collection of parsers for different datasets 
+# Datasets
+
+A comprehensive collection of parsers for different datasets
+
+## Requirements
+
+You'll need python 3.9 or above, we highly recomand to use [`virtualenv`](https://virtualenv.pypa.io/en/latest/)
+
+```
+virtualenv .env --python=3.9
+source ./.env/bin/activate
+pip install -r requirements.txt
+```
---
+++ datypes.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass, field
+from typing import List, Dict, Optional
+
+
+@dataclass
+class Image:
+    width: int
+    height: int
+    original_filename: str
+    filename: str
+    url: Optional[str] = None
+    thumbnail_url: Optional[str] = None
+    path: Optional[str] = None
+    workview_url: Optional[str] = None
+
+
+@dataclass
+class BoundingBox:
+    h: float
+    w: float
+    x: float
+    y: float
+
+
+@dataclass
+class Point:
+    x: float
+    y: float
+
+
+@dataclass
+class Polygon:
+    path: List[Point]
+
+
+@dataclass
+class Annotation:
+    name: str
+
+
+@dataclass
+class PolygonAnnotation(Annotation):
+    bounding_box: BoundingBox
+    polygon: Polygon
+
+
+@dataclass
+class TagAnnotation(Annotation):
+    tag: Dict = field(default_factory=dict)
+
+
+@dataclass
+class AnnotationFile:
+    dataset: str
+    image: Image
+    annotations: List[Annotation]
